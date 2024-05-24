f423f8f811ccc86f96c5cde9607ec499dbb2ab99
raulzitorrs123 <raulzito@outlook.com.br>
2024-05-24 16:25:12 -0300

    chore: adicionando bookSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminUserSeeder::class,
+            BookSeeder::class,
             // outros seeders, se houver
         ]);
     }
